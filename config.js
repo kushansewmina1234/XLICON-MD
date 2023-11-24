4798ba7016e0c75e3e0271f473f0a85f965dcfa2
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2023-11-24 20:24:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,26 +3,26 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "923184070915"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || "94725668277"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://salmanahmad:s4salmanyt@cluster0.szcj2eo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'xheikhsalman4422@gmail.com'
 global.github = 'https://github.com/salmanytofficial/XLICON-MD'
-global.location = 'Lahore Pak'
+global.location = 'asia/colombo'
 global.gurl = 'https://instagram.com/itz_shaikho/' // add your username
-global.sudo = process.env.SUDO || '923184070915' 
-global.devs = '923184070915';
+global.sudo = process.env.SUDO || '94772108460' 
+global.devs = '94725668277';
 global.website = 'https://github.com/salmanytofficial/XLICON-MD' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
 module.exports = {
-  sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
-  author:  process.env.PACK_AUTHER ||  '',
-  packname:  process.env.PACK_NAME || '',
+  sessionName:  process.env.SESSION_ID ||  "XLICON-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia09SVHhpdWREOWtOL3dVaVhNQ1RVeUZOLy9FditCZmM4NXUrVmdZaDhFdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUNZa3ZIb2xuKzlrS3dCSCtaVkVlZUQwK2g0bnAzcVNZbmpkeWFlR3lqOD0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJSnl5Lzl2UmNpV01hRURkeTRMeEpNc2EzeCtqUzA4Z2ROR2pZdWlNVlVzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ3VmNJU01NYTlwTmYwYkU4ZWN5WHB1ZkF1QVBscU83azN5VGNHeGowUGpzPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImtQck94Z3JtRkxDb1FTbklQQlFYaU9hZk96YUxjdVB3WjEzRXFoaHdtazQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlMrZ29SeUh5ampzNkNRYVFSNFRjSklQWmNBcTRwcFMyZ0kwRGlqSHN3MDA9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ2WExUMlYvR0FncUlJYTBGVjZ4UlhyZC9vNGQzUWpRVmNCaTZHbTZwZTZCY2piNk4zS1FBcFV5VytoV3plRVpKdDRtQmlNekEzY1VwT1BKZ3VYWUhoZz09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjE0NSwiYWR2U2VjcmV0S2V5IjoiMDdrbU1GUkxXajh2MHJXMEVteUwxUFNBQW5iVkNZSC9yWkxWcEFVZUR1bz0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoicFNfaDRHdnBRa09WWGpBSFZBX0NqQSIsInBob25lSWQiOiI2MzgzNWFmYi02ODVhLTRkOGMtYTBlYi03ZWQ5ZjM1YTgwM2QiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR2dGZ0J0MDg3NFFldEFWdEx6MzJrdlNPMmRnPSJ9LCJyZWdpc3RlcmVkIjpmYWxzZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWNloxb01BdkZ5dmFyeThLUnNBeWZyRU1nNTQ9In0sInJlZ2lzdHJhdGlvbiI6e30sImFjY291bnQiOnsiZGV0YWlscyI6IkNNWHh1OG9IRUozdmdxc0dHREE9IiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlF1Ly9PVlpiZGNaYWsyZlhIUEVkMmFSTzRoaTZlL2lraG1CRGlKVTh4bnM9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImFNeWQvL3Z5WUVyWE9NcW13RituNjYzcENPMFJkTmc4QlNWOTlEeThva2VMS0pUNmV4S1Byb0dHVUcxZWFBUnNQYms5VDZYOGpzS3BIdnBpdTdPZ0F3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJndk5JQnJQeGZDSHVBUWlqNHA1ZEVoblpoOXQ1VXpRV29WNFFzYzYybnkwc0NSZVRaMGNuaHRMZ25zcTZYNVdQTzdRSzlpKzFobGNwNDh0aHRtQWdoZz09In0sIm1lIjp7ImlkIjoiOTQ3MjU2NjgyNzc6NDZAcy53aGF0c2FwcC5uZXQifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3MjU2NjgyNzc6NDZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVUx2L3psV1czWEdXcE5uMXh6eEhkbWtUdUlZdW52NHBJWmdRNGlWUE1aNyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcwMDgzNzI4MH0=",  //PUT Session Id Here, 
+  author:  process.env.PACK_AUTHER ||  '@DARK SHAN',
+  packname:  process.env.PACK_NAME || 'KUSHAN',
    
-  botname:   process.env.BOT_NAME === undefined ? "Xlicon-Md" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'salmanytofficial' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  botname:   process.env.BOT_NAME === undefined ? "DARK SHAN" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'DARKSHAN' : process.env.OWNER_NAME,  
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
@@ -35,7 +35,7 @@ module.exports = {
   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
   BRANCH: 'main', 
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? 'HI IM ALIVE NOW' : process.env.ALIVE_MESSAGE,
   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
